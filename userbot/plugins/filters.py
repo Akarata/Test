e4ce2a54946880f3ca6e7211eb1727e820ff502e
Prastiko Sasmitho <prastiko46@gmail.com>
2020-12-08 09:22:17 +0700

    Update filters.py

--- userbot/plugins/filters.py
+++ userbot/plugins/filters.py
@@ -1,8 +1,10 @@
 # ported from paperplaneExtended by avinashreddy3108 for media support
 import re
 
+from telethon import events
+
+from .. import BOTLOG_CHATID, CMD_HELP, bot
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
-from . import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from .sql_helper.filter_sql import (
     add_filter,
     get_filters,
@@ -11,7 +13,7 @@ from .sql_helper.filter_sql import (
 )
 
 
-@bot.on(admin_cmd(incoming=True))
+@bot.on(events.NewMessage(incoming=True))
 async def filter_incoming_handler(handler):
     try:
         if not (await handler.get_sender()).bot:
@@ -36,14 +38,12 @@ async def filter_incoming_handler(handler):
 @bot.on(admin_cmd(pattern="filter (.*)"))
 @bot.on(sudo_cmd(pattern="filter (.*)", allow_sudo=True))
 async def add_new_filter(new_handler):
-    if new_handler.fwd_from:
-        return
     keyword = new_handler.pattern_match.group(1)
     string = new_handler.text.partition(keyword)[2]
     msg = await new_handler.get_reply_message()
     msg_id = None
     if msg and msg.media and not string:
-        if BOTLOG:
+        if BOTLOG_CHATID:
             await new_handler.client.send_message(
                 BOTLOG_CHATID,
                 f"#FILTER\
@@ -67,9 +67,9 @@ async def add_new_filter(new_handler):
     elif new_handler.reply_to_msg_id and not string:
         rep_msg = await new_handler.get_reply_message()
         string = rep_msg.text
-    success = "`Filter` **{}** `{} successfully`"
+    success = "`Filter` **{}** `{} ditambahkan`"
     if add_filter(str(new_handler.chat_id), keyword, string, msg_id) is True:
-        return await edit_or_reply(new_handler, success.format(keyword, "added"))
+        return await edit_or_reply(new_handler, success.format(keyword, "berhasil"))
     remove_filter(str(new_handler.chat_id), keyword)
     if add_filter(str(new_handler.chat_id), keyword, string, msg_id) is True:
         return await edit_or_reply(new_handler, success.format(keyword, "Updated"))
@@ -79,59 +79,61 @@ async def add_new_filter(new_handler):
 @bot.on(admin_cmd(pattern="filters$"))
 @bot.on(sudo_cmd(pattern="filters$", allow_sudo=True))
 async def on_snip_list(event):
-    if event.fwd_from:
-        return
-    OUT_STR = "There are no filters in this chat."
+    OUT_STR = "tidak ada filter pada pesan ini."
     filters = get_filters(event.chat_id)
     for filt in filters:
-        if OUT_STR == "There are no filters in this chat.":
-            OUT_STR = "Active filters in this chat:\n"
+        if OUT_STR == "Tidak ada filter di chat ini.":
+            OUT_STR = "Filter di chat ini:\n"
         OUT_STR += "👉 `{}`\n".format(filt.keyword)
-    await edit_or_reply(
-        event,
-        OUT_STR,
-        caption="Available Filters in the Current Chat",
-        file_name="filters.text",
-    )
+    if len(OUT_STR) > 4096:
+        with io.BytesIO(str.encode(OUT_STR)) as out_file:
+            out_file.name = "filters.text"
+            await bot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption="Filter yang tersedia di chat ini",
+                reply_to=event,
+            )
+            await event.delete()
+    else:
+        await edit_or_reply(event, OUT_STR)
 
 
 @bot.on(admin_cmd(pattern="stop (.*)"))
 @bot.on(sudo_cmd(pattern="stop (.*)", allow_sudo=True))
 async def remove_a_filter(r_handler):
-    if r_handler.fwd_from:
-        return
     filt = r_handler.pattern_match.group(1)
     if not remove_filter(r_handler.chat_id, filt):
-        await r_handler.edit("Filter` {} `doesn't exist.".format(filt))
+        await r_handler.edit("Filter` {} `tidak bisa.".format(filt))
     else:
-        await r_handler.edit("Filter `{} `was deleted successfully".format(filt))
+        await r_handler.edit("Filter `{} `sudah dihapus".format(filt))
 
 
 @bot.on(admin_cmd(pattern="rmfilters$"))
 @bot.on(sudo_cmd(pattern="rmfilters$", allow_sudo=True))
 async def on_all_snip_delete(event):
-    if event.fwd_from:
-        return
     filters = get_filters(event.chat_id)
     if filters:
         remove_all_filters(event.chat_id)
-        await edit_or_reply(event, f"filters in current chat deleted successfully")
+        await edit_or_reply(event, f"filter di chat ini sudah dihapus")
     else:
-        await edit_or_reply(event, f"There are no filters in this group")
+        await edit_or_reply(event, f"Tidak ada filter di grup ini")
 
 
 CMD_HELP.update(
     {
-        "filters": "__**PLUGIN NAME :** Filters__\
-    \n\n📌** CMD ➥** `.filters`\
-    \n**USAGE   ➥  **Lists all active (of your userbot) filters in a chat.\
-    \n\n📌** CMD ➥** `.filter`  reply to a message with .filter <keyword>\
-    \n**USAGE   ➥  **Saves the replied message as a reply to the 'keyword'.\
-    \nThe bot will reply to the message whenever 'keyword' is mentioned.\
-    \nWorks with everything from files to stickers.\
-    \n\n📌** CMD ➥** `.stop <keyword>`\
-    \n**USAGE   ➥  **Stops the specified keyword.\
-    \n\n📌** CMD ➥** `.rmfilters` \
-    \n**USAGE   ➥  **Removes all filters of your userbot in the chat."
+        "filters": "__**Nama Plugin:** Filters__\
+    \n\n✅** CMD ➥** `.filters`\
+    \n**Fungsi   ➥  **untuk melihat filter yang di gunakan pada userbot.\
+    \n\n✅** CMD ➥** `.filter`  balas pesan .filter <keyword>\
+    \n**Fungsi   ➥  **Untuk menyimpan filter 'katakunci'.\
+    \nBot akan membalas pesan ketika katakunci muncul.\
+    \nBisa pada media/stiker.\
+    \n\n✅** CMD ➥** `.stop <keyword>`\
+    \n**Fungsi   ➥  **Untuk menghapus katakunci.\
+    \n\n✅** CMD ➥** `.rmfilters` \
+    \n**Fungsi   ➥  **Hapus semua filter pada userbot."
     }
 )
