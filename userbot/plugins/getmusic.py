ae84ebab7495145c81dacd7b4add5d1f2dec3e8e
Prastiko Sasmitho <prastiko46@gmail.com>
2020-12-12 22:00:17 +0700

    Update getmusic.py

--- userbot/plugins/getmusic.py
+++ userbot/plugins/getmusic.py
@@ -18,6 +18,7 @@ from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.types import DocumentAttributeVideo
 
+from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot import CMD_HELP, bot
 from userbot.utils import progress
 
