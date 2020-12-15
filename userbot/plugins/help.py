500550be2ac6c097dbeed5ee5609ee28cbb7e8ed
Prastiko Sasmitho <prastiko46@gmail.com>
2020-12-15 14:23:23 +0700

    Update help.py

--- userbot/plugins/help.py
+++ userbot/plugins/help.py
@@ -18,13 +18,13 @@ async def cmd_list(event):
     input_str = event.pattern_match.group(1)
     if input_str == "text":
         string = (
-            "Total {count} commands found in {plugincount} plugins of catuserbot\n\n"
+            "Total {count} perintah ditemukan di {plugincount} plugin dari jichu_userbot\n\n"
         )
         catcount = 0
         plugincount = 0
         for i in sorted(CMD_LIST):
             plugincount += 1
-            string += f"{plugincount}) Commands found in Plugin " + i + " are \n"
+            string += f"{plugincount}) Perintah ditemukan di Plugin " + i + " oleh \n"
             for iter_list in CMD_LIST[i]:
                 string += "    " + str(iter_list)
                 string += "\n"
@@ -41,14 +41,14 @@ async def cmd_list(event):
                 .get("key")
             )
             url = f"https://nekobin.com/{key}"
-            reply_text = f"**All commands of the catuserbot can be seen [here]({url})**"
+            reply_text = f"**Semua perintah jichu userbot dapat dilihat [disini]({url})**"
             await event.edit(reply_text)
             return
         await event.edit(string.format(count=catcount, plugincount=plugincount))
         return
     if input_str:
         if input_str in CMD_LIST:
-            string = "<b>{count} Commands found in plugin {input_str}:</b>\n\n"
+            string = "<b>{count} Perintah ditemukan di plugin {input_str}:</b>\n\n"
             catcount = 0
             for i in CMD_LIST[input_str]:
                 string += f"  •  <code>{i}</code>"
@@ -63,9 +63,9 @@ async def cmd_list(event):
             await event.delete()
     else:
         if HELPTYPE is True:
-            help_string = f"Userbot Helper. Provided by {mention} to reveal all the plugins\
-                          \nCheck `.help plugin name` for commands, in case popup doesn't appear.\
-                          \nCheck `.info plugin name` for usage of thoose plugins and commands"
+            help_string = f"Pembantu Userbot. Disediakan oleh {mention} untuk menampilkan semua plugin\
+                          \nCek `.help nama plugin` untuk perintah, jika popup tidak muncul.\
+                          \nCek `.info nama plugin` untuk penggunaan plugin dan perintah tersebut"
             tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
@@ -73,12 +73,12 @@ async def cmd_list(event):
             await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
             await event.delete()
         else:
-            string = "<b>Please specify which plugin do you want help for !!\
-                \nNumber of plugins : </b><code>{count}</code>\
-                \n<b>Usage:</b> <code>.help plugin name</code> \n\n"
+            string = "<b>Harap tentukan plugin mana yang Anda inginkan bantuannya !!\
+                \nJumlah Plugin : </b><code>{count}</code>\
+                \n<b>Gunakan:</b> <code>.help nama plugin</code> \n\n"
             catcount = 0
             for i in sorted(CMD_LIST):
-                string += "◆ " + f"<code>{str(i)}</code>"
+                string += "• " + f"<code>{str(i)}</code>"
                 string += " "
                 catcount += 1
             await event.edit(string.format(count=catcount), parse_mode="HTML")
